2e07be7d5b09de8164d6c348a97c29a507d46b9c
ansk1d <anasalkhaled@aol.com>
2022-06-17 21:29:47 +0300

    lets go

--- Ninja_GoldApp/urls.py
+++ Ninja_GoldApp/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('findgold', views.findgold),
+    path('luckgold', views.luckgold)
 ]
--- Ninja_GoldApp/views.py
+++ Ninja_GoldApp/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+import random
 
 def index(request):
-    return render(request, "index.html")
+    if 'mygold' not in request.session:
+        request.session['mygold'] =0
+    context = {
+        "gold" :request.session['mygold']
+    }
+    return render(request, "index.html", context)
+
+def findgold(request):
+    randomnum = random.randint(10,20)
+    request.session['mygold']+=randomnum
+    return redirect('/')
+
+def luckgold(request):
+    randomnum = random.randint(-50,50)
+    request.session['mygold']+=randomnum
+    return redirect('/')
